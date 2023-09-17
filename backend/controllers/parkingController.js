9de0c46af552cbd74d3222395a96bb8d6a31cb21
Nigam <nigam21nir@gmail.com>
2023-09-17 15:26:16 +0545

    Minor Refactoring in User side

--- backend/controllers/parkingController.js
+++ backend/controllers/parkingController.js
@@ -1,40 +1,48 @@
-const ParkingLocation=require("../models/locationModel")
- const UserReservation=require("../models/parkingModel")
+const ParkingLocation = require("../models/locationModel");
+const UserReservation = require("../models/parkingModel");
 const ErrorHandler = require("../utils/errorHandler");
 const tryCatchAsync = require("../middleware/catchAsyncError");
 
-exports.registerLocation=tryCatchAsync(async (req,res,next)=>{
-  const { name, address,latitude,longitude, parkingSpaces } = req.body;
-// Create a new parking location
-const newLocation = new ParkingLocation({
-  name,
-  address,
-  latitude,
-  longitude,
-  parkingSpaces,
+exports.registerLocation = tryCatchAsync(async (req, res, next) => {
+  const { name, address, latitude, longitude, parkingSpaces } = req.body;
+  // Create a new parking location
+  const newLocation = new ParkingLocation({
+    name,
+    address,
+    latitude,
+    longitude,
+    parkingSpaces,
+  });
+
+  // Save the new location to the database
+  await newLocation.save();
+  res.json({ message: "Location saved successfully" });
 });
 
-// Save the new location to the database
-await newLocation.save();
-res.json({ message: 'Location saved successfully' });
-})
-
 exports.confirmparking = tryCatchAsync(async (req, res, next) => {
-  const { userId, locationId, vehicleType, startTime, parkingtime,reservedSpaceNumber } = req.body;
-console.log(startTime)
-console.log(parkingtime)
+  const {
+    userId,
+    locationId,
+    vehicleType,
+    startTime,
+    parkingtime,
+    reservedSpaceNumber,
+  } = req.body;
+  console.log(startTime);
+  console.log(parkingtime);
   // Calculate the expiration time based on the start time and duration
   const startTimehere = new Date(req.body.startTime); // Convert the provided startTime to a Date object
   const durationInMinuteshere = req.body.parkingtime;
-  
-  // Calculate the expiration time by adding the duration in minutes to the start time
-  const expirationTime = new Date(startTimehere.getTime() + parkingtime * 60000);
-  
-  console.log(expirationTime);
-  
 
-console.log(startTimehere)
-console.log(expirationTime)
+  // Calculate the expiration time by adding the duration in minutes to the start time
+  const expirationTime = new Date(
+    startTimehere.getTime() + parkingtime * 60000
+  );
+
+  console.log(expirationTime);
+
+  console.log(startTimehere);
+  console.log(expirationTime);
   try {
     // Check if the requested parking space is available
     const parkingLocation = await ParkingLocation.findById(locationId);
@@ -46,7 +54,7 @@ console.log(expirationTime)
     const parkingSpace = parkingLocation.parkingSpaces.find(
       (space) => space.spaceNumber === reservedSpaceNumber
     );
-console.log(parkingSpace)
+    console.log(parkingSpace);
     if (!parkingSpace || parkingSpace.isOccupied) {
       return next(new ErrorHandler("No parking space is available", 400));
     }
@@ -67,7 +75,7 @@ console.log(parkingSpace)
 
     await reservation.save();
 
-    return res.status(201).json({ message: 'Booking successful.' });
+    return res.status(201).json({ message: "Booking successful." });
   } catch (error) {
     return next(new ErrorHandler(error.message, 500));
   }
@@ -86,7 +94,7 @@ exports.getAvailableParkingSpaces = tryCatchAsync(async (req, res, next) => {
 
     // Filter the parking spaces based on vehicle type and occupancy status
     const availableSpaces = parkingLocation.parkingSpaces.filter(
-      (space) => space.vehicleType === vehicleType && space.isOccupied==false
+      (space) => space.vehicleType === vehicleType && space.isOccupied == false
     );
 
     return res.status(200).json({ availableSpaces });
@@ -95,9 +103,9 @@ exports.getAvailableParkingSpaces = tryCatchAsync(async (req, res, next) => {
   }
 });
 
-exports.mybookingDetails=tryCatchAsync(async (req,res,next)=>{
-  const {userId}=req.body
-  UserReservation.find({userId:userId}).populate('userId').then(p=>console.log(p))
-})
-
-
+exports.mybookingDetails = tryCatchAsync(async (req, res, next) => {
+  const { userId } = req.body;
+  UserReservation.find({ userId: userId })
+    .populate("userId")
+    .then((p) => console.log(p));
+});
