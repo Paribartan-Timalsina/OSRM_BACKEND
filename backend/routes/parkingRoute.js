9a7929a3e173fcbe712ed9ec2428f9e1d6c5991e
Nigam <nigam21nir@gmail.com>
2023-09-21 12:39:36 +0545

    Solved Faults in merge

--- backend/routes/parkingRoute.js
+++ backend/routes/parkingRoute.js
@@ -21,16 +21,3 @@ router
   .get(getAllReviews)
   .delete(isUserAuthenticated, deleteReview);
 module.exports = router;
-
-const router = express.Router();
-const {
-  confirmparking,
-  registerLocation,
-  getAvailableParkingSpaces,
-  mybookingDetails,
-} = require("../controllers/parkingController");
-router.route("/bookparking").post(confirmparking);
-router.route("/registerlocation").post(registerLocation);
-router.route("/availableparkings").post(getAvailableParkingSpaces);
-router.route("/mybookingdetails").get(mybookingDetails);
-module.exports = router;
