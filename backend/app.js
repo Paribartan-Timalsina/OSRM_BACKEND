c57616cfe8b17f2cb971662bc5d4e6cc15b6e748
Paribartan <timalsinapari015@gmail.com>
2023-09-21 11:35:15 +0545

    Added almsot all the backend functionality

---
+++ backend/app.js
@@ -0,0 +1,19 @@
+//import express and export it as the express function
+const express = require("express");
+const app = express();
+const errorMiddleware = require("./middleware/error");
+//const products = require("./routes/productRoute");
+const user = require("./routes/userRoutes");
+const parking=require("./routes/parkingRoute");
+const cookieParser = require("cookie-parser");
+//const cron=require("./controllers/updateReservation")
+app.use(express.json());
+app.use(cookieParser());
+
+//Route Imports
+//cron.start();
+app.use("/user", user);
+app.use("/parking", parking);
+app.use(errorMiddleware);
+
+module.exports = app;
